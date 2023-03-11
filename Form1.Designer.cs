b4c4c9f698a2a85a2e3bfbecad5dfea14c58eb07
Juan Armando Vallejo <73964259+ArmandoVallejo@users.noreply.github.com>
2023-03-10 20:22:58 -0600

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace ProyeU2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDes = new System.Windows.Forms.TextBox();
+            this.tbPre = new System.Windows.Forms.TextBox();
+            this.rTB = new System.Windows.Forms.RichTextBox();
+            this.tbTot = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbRec = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbSob = new System.Windows.Forms.TextBox();
+            this.btAgr = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Canti = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.accionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.limpiarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Canti)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripcion";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(260, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio";
+            // 
+            // tbDes
+            // 
+            this.tbDes.Location = new System.Drawing.Point(20, 55);
+            this.tbDes.Margin = new System.Windows.Forms.Padding(2);
+            this.tbDes.MaxLength = 8;
+            this.tbDes.Name = "tbDes";
+            this.tbDes.Size = new System.Drawing.Size(80, 20);
+            this.tbDes.TabIndex = 3;
+            // 
+            // tbPre
+            // 
+            this.tbPre.Location = new System.Drawing.Point(246, 55);
+            this.tbPre.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPre.Name = "tbPre";
+            this.tbPre.Size = new System.Drawing.Size(76, 20);
+            this.tbPre.TabIndex = 5;
+            this.tbPre.TextChanged += new System.EventHandler(this.tbPre_TextChanged);
+            // 
+            // rTB
+            // 
+            this.rTB.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.rTB.Enabled = false;
+            this.rTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rTB.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rTB.Location = new System.Drawing.Point(20, 101);
+            this.rTB.Margin = new System.Windows.Forms.Padding(2);
+            this.rTB.Name = "rTB";
+            this.rTB.Size = new System.Drawing.Size(479, 186);
+            this.rTB.TabIndex = 6;
+            this.rTB.Text = "";
+            // 
+            // tbTot
+            // 
+            this.tbTot.Enabled = false;
+            this.tbTot.Location = new System.Drawing.Point(521, 122);
+            this.tbTot.Margin = new System.Windows.Forms.Padding(2);
+            this.tbTot.Name = "tbTot";
+            this.tbTot.Size = new System.Drawing.Size(91, 20);
+            this.tbTot.TabIndex = 7;
+            this.tbTot.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(518, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total a pagar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(537, 157);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Recibido";
+            // 
+            // tbRec
+            // 
+            this.tbRec.Location = new System.Drawing.Point(525, 176);
+            this.tbRec.Margin = new System.Windows.Forms.Padding(2);
+            this.tbRec.Name = "tbRec";
+            this.tbRec.Size = new System.Drawing.Size(86, 20);
+            this.tbRec.TabIndex = 10;
+            this.tbRec.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(537, 214);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sobrante";
+            // 
+            // tbSob
+            // 
+            this.tbSob.Enabled = false;
+            this.tbSob.Location = new System.Drawing.Point(526, 233);
+            this.tbSob.Margin = new System.Windows.Forms.Padding(2);
+            this.tbSob.Name = "tbSob";
+            this.tbSob.Size = new System.Drawing.Size(86, 20);
+            this.tbSob.TabIndex = 12;
+            this.tbSob.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btAgr
+            // 
+            this.btAgr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAgr.Location = new System.Drawing.Point(527, 273);
+            this.btAgr.Margin = new System.Windows.Forms.Padding(2);
+            this.btAgr.Name = "btAgr";
+            this.btAgr.Size = new System.Drawing.Size(86, 28);
+            this.btAgr.TabIndex = 13;
+            this.btAgr.Text = "Agregar";
+            this.btAgr.UseVisualStyleBackColor = true;
+            this.btAgr.Click += new System.EventHandler(this.btAgr_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(26, 82);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(461, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Descripcion         Cantidad     Precio     Precio Total     Precio Total C/Iva";
+            // 
+            // Canti
+            // 
+            this.Canti.Location = new System.Drawing.Point(137, 56);
+            this.Canti.Name = "Canti";
+            this.Canti.ReadOnly = true;
+            this.Canti.Size = new System.Drawing.Size(86, 20);
+            this.Canti.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(524, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 18);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "$";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(526, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 18);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "$";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(526, 234);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 18);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "$";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(635, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // accionesToolStripMenuItem
+            // 
+            this.accionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imprimirToolStripMenuItem,
+            this.limpiarToolStripMenuItem});
+            this.accionesToolStripMenuItem.Name = "accionesToolStripMenuItem";
+            this.accionesToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.accionesToolStripMenuItem.Text = "Acciones";
+            // 
+            // imprimirToolStripMenuItem
+            // 
+            this.imprimirToolStripMenuItem.Name = "imprimirToolStripMenuItem";
+            this.imprimirToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.imprimirToolStripMenuItem.Text = "Imprimir";
+            this.imprimirToolStripMenuItem.Click += new System.EventHandler(this.imprimirToolStripMenuItem_Click);
+            // 
+            // limpiarToolStripMenuItem
+            // 
+            this.limpiarToolStripMenuItem.Name = "limpiarToolStripMenuItem";
+            this.limpiarToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.limpiarToolStripMenuItem.Text = "Limpiar";
+            this.limpiarToolStripMenuItem.Click += new System.EventHandler(this.limpiarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(635, 324);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Canti);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btAgr);
+            this.Controls.Add(this.tbSob);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbRec);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbTot);
+            this.Controls.Add(this.rTB);
+            this.Controls.Add(this.tbPre);
+            this.Controls.Add(this.tbDes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Canti)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDes;
+        private System.Windows.Forms.TextBox tbPre;
+        private System.Windows.Forms.RichTextBox rTB;
+        private System.Windows.Forms.TextBox tbTot;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbRec;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbSob;
+        private System.Windows.Forms.Button btAgr;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown Canti;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem accionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem limpiarToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyeU2
+{
+    public partial class Form1 : Form
+    {
+        List<double> list = new List<double>(); //Lista para almacenar los precios totales
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbPre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btAgr_Click(object sender, EventArgs e)
+        {
+            double precio = 0;
+            if ((tbDes.Equals("") || Canti.Value == 0 || tbPre.Equals("")))
+            {
+                //no estan llenados los 3 campos
+                MessageBox.Show("No estan llenados los 3 campos");
+            }
+            else
+            {
+                //Formato
+                //Descripcion_Cantidad_Precio_Precio Total_Precio Total C/Iva
+                try
+                {
+                precio = int.Parse(tbPre.Text);
+                list.Add(((double)Canti.Value * precio) * 1.16);
+                rTB.Text += tbDes.Text + "\t\t\t" + Canti.Value + "\t" + tbPre.Text + "\t\t" + ((double)Canti.Value * precio) + "\t\t" + (((double)Canti.Value * precio) * 1.16) + "\n";
+
+                double totem = 0;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    totem += list.ElementAt(i);
+                }
+                tbTot.Text = totem.ToString();
+            }catch (Exception ex) {
+                    MessageBox.Show("Debes ingresar solo numeros en el precio");
+                }
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imprimirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+            btAgr.Enabled = false;
+            double total = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                total += list.ElementAt(i); ;
+            }
+            tbSob.Text = (double.Parse(tbRec.Text) - total).ToString();
+        }catch(Exception ex)
+            {
+                MessageBox.Show("Debes ingresar solo cantidades numericas en el recibido");
+            }
+        }
+        private void limpiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTB.Clear();
+            list.Clear();
+            tbDes.Clear();
+            Canti.Value = 0;
+            tbPre.Clear();
+            tbTot.Clear();
+            btAgr.Enabled = true;
+            tbRec.Clear();
+            tbSob.Clear();
+        }
+    }
+}
